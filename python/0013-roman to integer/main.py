bf1d400d529272b175fb9ddc00711ac07c752149
xiaoC <dalen0822@163.com>
2019-08-04 22:12:40 +0800

    Update main.py

--- python/0013-roman to integer/main.py
+++ python/0013-roman to integer/main.py
@@ -3,24 +3,11 @@ class Solution:
         d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         len1 = len(s)
         sum = 0
-        flag = False
-        for i in range(len1 - 1):
-            if flag == True:
-                flag = False
-                continue
-            if s[i] == 'I' and (s[i+1] == 'V' or s[i+1] == 'X'):
-                flag = True
-                sum += d[s[i+1]] - 1
-            elif s[i] == 'X' and (s[i+1] == 'L' or s[i+1] == 'C'):
-                flag = True
-                sum += d[s[i+1]] - 10
-            elif s[i] == 'C' and (s[i+1] == 'D' or s[i+1] == 'M'):
-                flag = True
-                sum += d[s[i+1]] - 100
+        for i in range(len1):
+            if i < len1 - 1 and d[s[i]] < d[s[i+1]]:
+                sum -= d[s[i]]
             else:
                 sum += d[s[i]]
-        if flag == False:
-            sum += d[s[len1-1]]
         return sum
 
 
